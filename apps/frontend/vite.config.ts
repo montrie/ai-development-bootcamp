ee8330048f239f5a1c0b09b29dc20f412eb1d649
Jonas Felber <jonas.felber@senacor.com>
2026-04-30 17:34:37 +0200

    Migrate project to use docker

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -11,8 +11,12 @@ export default defineConfig({
   },
   server: {
     port: 5173,
+    host: process.env.VITE_HOST === 'true',
+    watch: {
+      usePolling: process.env.VITE_POLLING === 'true',
+    },
     proxy: {
-      '/api': 'http://localhost:8080',
+      '/api': process.env.BACKEND_URL ?? 'http://localhost:8080',
     },
   },
 });
